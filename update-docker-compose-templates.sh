14380b9058ce511eeb244b8cd459380b8bdbe6a0
TorGuard Ops <ops@torguard.net>
2022-11-08 04:01:59 +0000

    Update docker-compose templates

--- update-docker-compose-templates.sh
+++ update-docker-compose-templates.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-# Update script for DockerDeploy in PrivateRouter OpenWRT routers
+# Update script for docker-compose-templates in PrivateRouter OpenWRT routers
 # Version 1.0
 # Contact: ops@torguard.net
 
-HASH_STORE="/etc/config/.dockerdeploy"
-GIT_URL="https://github.com/PrivateRouter-LLC/dockerdeploy.git"
-TMP_DIR="/tmp/dockerdeploy"
-DOCKERDEPLOY_LOCATION="/usr/bin/dockerdeploy"
+HASH_STORE="/etc/config/.docker-compose-templates"
+GIT_URL="https://github.com/PrivateRouter-LLC/docker-compose-templates"
+TMP_DIR="/tmp/docker-compose-templates"
+TEMPLATE_LOCATION="/root/docker-compose"
 UPDATE_NEEDED="0"
 
 CURRENT_HASH=$(
     curl \
-        cd --silent https://api.github.com/repos/PrivateRouter-LLC/dockerdeploy/commits/main |
+        cd --silent https://api.github.com/repos/PrivateRouter-LLC/docker-compose-templates/commits/main |
         jq --raw-output '.sha'
 )
 
@@ -46,15 +46,14 @@ if [[ "${UPDATE_NEEDED}" == "1" ]]; then
     echo "Cloning ${GIT_URL} into ${TMP_DIR}"
     git clone --depth=1 "${GIT_URL}" "${TMP_DIR}"
 
-    [ -f "${DOCKERDEPLOY_LOCATION}" ] && { echo "Removing old ${DOCKERDEPLOY_LOCATION}"; rm "${DOCKERDEPLOY_LOCATION}"; }
-    [ -f "${TMP_DIR}/dockerdeploy.sh" ] && {
-        echo "Moving ${TMP_DIR}/dockerdeploy.sh to ${DOCKERDEPLOY_LOCATION}"
-        mv "${TMP_DIR}/dockerdeploy.sh" "${DOCKERDEPLOY_LOCATION}"   
-        chmod +x "${DOCKERDEPLOY_LOCATION}"
+    [ -d "${TEMPLATE_LOCATION}" ] && { echo "Removing old ${TEMPLATE_LOCATION}"; rm -rf "${TEMPLATE_LOCATION}"; }
+    [ -d "${TMP_DIR}/docker-compose" ] && {
+        echo "Moving ${TMP_DIR}/docker-compose to ${TEMPLATE_LOCATION}"
+        mv "${TMP_DIR}/docker-compose" "${TEMPLATE_LOCATION}"   
     }
 
     echo "Removing ${TMP_DIR} as cleanup"
     rm -rf "${TMP_DIR}"
 else
-    echo "No update needed for ${DOCKERDEPLOY_LOCATION}"
+    echo "No update needed for ${TEMPLATE_LOCATION}"
 fi
