3236d8e32bd9fd95ee60faf5e800fbb065c955e3
TorGuard Ops <ops@torguard.net>
2022-11-22 04:02:21 +0000

    Added first-run.sh script

---
+++ first-run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/eval bash
+# This are the commands run on router first steup to update the scripts
+
+bash /root/update-scripts/update-docker-compose-templates.sh
+bash /root/update-scripts/update-dockerdeploy.sh
